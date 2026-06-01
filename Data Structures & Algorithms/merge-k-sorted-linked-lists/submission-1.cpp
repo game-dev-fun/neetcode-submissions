9ecf569afa375dd9e82e40e858f1af07b3855fb6
shivam <94660222+game-dev-fun@users.noreply.github.com>
2026-06-01 21:09:54 +0530

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        std::vector<ListNode*> curr(lists);
+        std::erase(curr,nullptr);
+        if(curr.empty()) return nullptr;
+        ListNode dummy;
+        ListNode* listCur{&dummy};
+        while(true)
+        {
+            int minIndex{0};
+            for(int i{1}; i < curr.size(); ++i)
+            {
+                if(curr[minIndex]->val > curr[i]->val)
+                {
+                    minIndex = i;
+                }
+            }
+            listCur->next = new ListNode(curr[minIndex]->val);
+            listCur = listCur->next;
+            if(!curr[minIndex]->next)
+            {
+                curr.erase(curr.begin()+minIndex);
+            }
+            else
+            {
+                curr[minIndex] = curr[minIndex]->next;
+            }
+            if(curr.size() == 1)
+            {
+                listCur->next = curr[0];
+                break;
+            }
+            else if(curr.empty()) break;
+        }
+        return dummy.next;   
+    }
+};
